bf069248eb793fd981499421b7da122affdce7a9
Bharath-339 <89066267+Bharath-339@users.noreply.github.com>
2026-04-19 16:31:35 +0530

    Add streams for other formats (json and csv)

--- main.py
+++ main.py
@@ -36,6 +36,45 @@ def define_udf():
         ),
     }
 
+def format_text_df(text_df):
+
+    text_df = text_df.withColumn("file_name", regexp_replace(udfs["extract_file_name_udf"]("value"), "\r", ""))
+    text_df = text_df.withColumn("position", regexp_replace(udfs["extract_position_udf"]("value"), "\r", ""))
+    text_df = text_df.withColumn("classcode", regexp_replace(udfs["extract_classcode_udf"]("value"), "\r", ""))
+    text_df = text_df.withColumn("salary_start", udfs["extract_salary_start_udf"]("value").getItem("salary_start"))
+    text_df = text_df.withColumn("salary_end", udfs["extract_salary_start_udf"]("value").getItem("salary_end"))
+    text_df = text_df.withColumn("start_date", udfs["extract_start_date_udf"]("value"))
+    text_df = text_df.withColumn("end_date", udfs["extract_end_date_udf"]("value"))
+    text_df = text_df.withColumn("req", regexp_replace(udfs["extract_requirements_udf"]("value"), "\r", ""))
+    text_df = text_df.withColumn("notes", regexp_replace(udfs["extract_notes_udf"]("value"), "\r", ""))
+    text_df = text_df.withColumn("duties", regexp_replace(udfs["extract_duties_udf"]("value"), "\r", ""))
+    text_df = text_df.withColumn("selection", regexp_replace(udfs["extract_selection_udf"]("value"), "\r", ""))
+    text_df = text_df.withColumn("experience_length",
+                                 regexp_replace(udfs["extract_experience_length_udf"]("value"), "\r", ""))
+    text_df = text_df.withColumn("job_type", regexp_replace(udfs["extract_job_type_udf"]("value"), "\r", ""))
+    text_df = text_df.withColumn("education_length",
+                                 regexp_replace(udfs["extract_education_length_udf"]("value"), "\r", ""))
+    text_df = text_df.withColumn("school_type", regexp_replace(udfs["extract_school_type_udf"]("value"), "\r", ""))
+    text_df = text_df.withColumn("application_location",
+                                 regexp_replace(udfs["extract_application_location_udf"]("value"), "\r", ""))
+    return text_df.select(
+        "file_name",
+        "position",
+        "classcode",
+        "salary_start",
+        "salary_end",
+        "start_date",
+        "end_date",
+        "req",
+        "notes",
+        "duties",
+        "selection",
+        "experience_length",
+        "job_type",
+        "education_length",
+        "school_type",
+        "application_location",
+    )
 
 if __name__ == "__main__":
     spark = (
@@ -83,43 +122,26 @@ if __name__ == "__main__":
     )
     udfs = define_udf()
 
-    jobs_df = (
-        spark.readStream.format("text").option("wholetext", "true").load(text_input_dir)
-    )
-    jobs_df = jobs_df.withColumn("file_name", regexp_replace(udfs["extract_file_name_udf"]("value"), "\r", ""))
-    jobs_df = jobs_df.withColumn("position", regexp_replace(udfs["extract_position_udf"]("value"), "\r", ""))
-    jobs_df = jobs_df.withColumn("classcode", regexp_replace(udfs["extract_classcode_udf"]("value"), "\r", ""))
-    jobs_df = jobs_df.withColumn("salary_start", udfs["extract_salary_start_udf"]("value").getItem("salary_start"))
-    jobs_df = jobs_df.withColumn("salary_end", udfs["extract_salary_start_udf"]("value").getItem("salary_end"))
-    jobs_df = jobs_df.withColumn("start_date", udfs["extract_start_date_udf"]("value"))
-    jobs_df = jobs_df.withColumn("end_date", udfs["extract_end_date_udf"]("value"))
-    jobs_df = jobs_df.withColumn("req", regexp_replace(udfs["extract_requirements_udf"]("value"), "\r", ""))
-    jobs_df = jobs_df.withColumn("notes", regexp_replace(udfs["extract_notes_udf"]("value"), "\r", ""))
-    jobs_df = jobs_df.withColumn("duties", regexp_replace(udfs["extract_duties_udf"]("value"), "\r", ""))
-    jobs_df = jobs_df.withColumn("selection", regexp_replace(udfs["extract_selection_udf"]("value"), "\r", ""))
-    jobs_df = jobs_df.withColumn("experience_length", regexp_replace(udfs["extract_experience_length_udf"]("value"), "\r", ""))
-    jobs_df = jobs_df.withColumn("job_type", regexp_replace(udfs["extract_job_type_udf"]("value"), "\r", ""))
-    jobs_df = jobs_df.withColumn("education_length", regexp_replace(udfs["extract_education_length_udf"]("value"), "\r", ""))
-    jobs_df = jobs_df.withColumn("school_type", regexp_replace(udfs["extract_school_type_udf"]("value"), "\r", ""))
-    jobs_df = jobs_df.withColumn("application_location", regexp_replace(udfs["extract_application_location_udf"]("value"), "\r", ""))
-    jobs_df = jobs_df.select(
-        "file_name",
-        "position",
-        "classcode",
-        "salary_start",
-        "salary_end",
-        "start_date",
-        "end_date",
-        "req",
-        "notes",
-        "duties",
-        "selection",
-        "experience_length",
-        "job_type",
-        "education_length",
-        "school_type",
-        "application_location",
-    )
-    query = jobs_df.writeStream.format("console").outputMode("append").start()
-    # Stop the SparkSession
+    text_df = spark.readStream.format("text").option("wholetext", "true").load(text_input_dir)
+    json_df = spark.readStream.format("json").schema(schema).load(json_input_dir)
+    csv_df = spark.readStream.format("csv").option("header", "true").schema(schema).load(csv_input_dir)
+
+    # Apply UDFs to the text DataFrame to extract structured fields
+    text_df = format_text_df(text_df)
+
+    final_df = text_df.unionByName(json_df).unionByName(csv_df)
+
+    def streamWriter(df, output_dir):
+        return (df.writeStream
+                    .format("parquet")
+                    .trigger("processingTime", "2 minute")
+                    .option("checkpointLocation", f'{output_dir}/checkpoint')
+                    .outputMode("append")
+                    .start(f'{output_dir}/data')
+                )
+
+    s3_bucket_name = configuration.get("S3_BUCKET_NAME")
+    s3_dir = f's3a://{s3_bucket_name}/unstructured_streaming/'
+
+    query = streamWriter(final_df,s3_dir)
     query.awaitTermination()
